f7222b5f7a73d39bd53349677054ee648325b31d
Mobark Walid <Mobarkbacran1088@gmail.com>
2023-08-15 00:45:59 +0300

    Added Env File

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import "package:flutter/material.dart";
+import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:lockin/home.dart";
 import "package:lol_api/lol_api.dart";
 
-void main() async {
-  RiotApi.init(apiKey: "RGAPI-b235f7e6-31d6-466b-9532-d707704a7f80");
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  String? RiotApiKey = dotenv.env['API_KEY'];
+  RiotApi.init(apiKey: RiotApiKey ?? "");
   runApp(MyApp());
 }
 
